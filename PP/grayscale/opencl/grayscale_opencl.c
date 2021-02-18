533e3c16a29d4650dd84a5d429a3f6a675108df7
Martin Vassilev <mrtn.vassilev@gmail.com>
2021-02-18 14:18:39 +0200

    Fix comments.

--- PP/grayscale/opencl/grayscale_opencl.c
+++ PP/grayscale/opencl/grayscale_opencl.c
@@ -36,40 +36,40 @@
      _ret;                                                                      \
   })
 
-// четене на изображение от външен файл
+// Четене на изображение от външен файл
 static PPMImage *ReadPPM(const char *filename) {
      char buff[16];
      PPMImage *img;
      FILE *fp;
      int c, rgb_comp_color;
 
-     // зареждане на PPM файла за четене
+     // Зареждане на PPM файла за четене
      fp = fopen(filename, "rb");
      if (!fp) {
           fprintf(stderr, "Unable to open file '%s'\n", filename);
           exit(1);
      }
 
-     // четене на метаданните от него 
+     // Четене на метаданните от него 
      if (!fgets(buff, sizeof(buff), fp)) {
           perror(filename);
           exit(1);
      }
 
-     // проверка на метаданните
+     // Проверка на метаданните
      if (buff[0] != 'P' || buff[1] != '6') {
           fprintf(stderr, "Invalid image format (must be 'P6')\n");
           exit(1);
      }
 
-     // заделяне на памет
+     // Заделяне на памет
      img = (PPMImage *)malloc(sizeof(PPMImage));
      if (!img) {
           fprintf(stderr, "Unable to allocate memory\n");
           exit(1);
      }
 
-     // проверка за коментари вътре в самото изображение
+     // Проверка за коментари вътре в самото изображение
      c = getc(fp);
      while (c == '#') {
           while (getc(fp) != '\n') ;
@@ -77,26 +77,26 @@ static PPMImage *ReadPPM(const char *filename) {
      }
 
      ungetc(c, fp);
-     // проверка на данните за размера на изображението
+     // Проверка на данните за размера на изображението
      if (fscanf(fp, "%d %d", &img->x, &img->y) != 2) {
           fprintf(stderr, "Invalid image size (error loading '%s')\n", filename);
           exit(1);
      }
 
-     // проверка на RGB компонента
+     // Проверка на RGB компонента
      if (fscanf(fp, "%d", &rgb_comp_color) != 1) {
           fprintf(stderr, "Invalid rgb component (error loading '%s')\n", filename);
           exit(1);
      }
 
-     // проверка на размерността на RGB компонента
+     // Проверка на размерността на RGB компонента
      if (rgb_comp_color!= RGB_COMPONENT_COLOR) {
           fprintf(stderr, "'%s' does not have 8-bits components\n", filename);
           exit(1);
      }
 
      while (fgetc(fp) != '\n') ;
-     // заделяне на памет за информацията във всеки пиксел
+     // Заделяне на памет за информацията във всеки пиксел
      img->data = (PPMPixel*)malloc(img->x * img->y * sizeof(PPMPixel));
 
      if (!img) {
@@ -104,7 +104,7 @@ static PPMImage *ReadPPM(const char *filename) {
           exit(1);
      }
 
-     // зареждане на данните за всеки пиксел
+     // Зареждане на данните за всеки пиксел
      if (fread(img->data, 3 * img->x, img->y, fp) != img->y) {
           fprintf(stderr, "Error loading image '%s'\n", filename);
           exit(1);
@@ -114,34 +114,34 @@ static PPMImage *ReadPPM(const char *filename) {
      return img;
 }
 
-// запис на изображение във външен файл
+// Запис на изображение във външен файл
 void WritePPM(const char *filename, PPMImage *img) {
      FILE *fp;
-     // отваряне на файл в режим за писане
+     // Отваряне на файл в режим за писане
      fp = fopen(filename, "wb");
      if (!fp) {
           fprintf(stderr, "Unable to open file '%s'\n", filename);
           exit(1);
      }
 
-     // записване на метаданни за типа на PPM изображението
+     // Записване на метаданни за типа на PPM изображението
      fprintf(fp, "P6\n");
 
-     // запис на коментари
+     // Запис на коментари
      fprintf(fp, "# Created by %s\n", CREATOR);
 
      // запис на размера на изображението
      fprintf(fp, "%d %d\n",img->x,img->y);
 
-     // запис на размерността на RGB компонента
+     // Запис на размерността на RGB компонента
      fprintf(fp, "%d\n", RGB_COMPONENT_COLOR);
 
-     // запис на данните за пикселите от изображението
+     // Запис на данните за пикселите от изображението
      fwrite(img->data, 3 * img->x, img->y, fp);
      fclose(fp);
 }
 
-// четене на сорс кода на kernel функцията от външен файл
+// Четене на сорс кода на kernel функцията от външен файл
 char *ReadKernelProgram() {
      FILE *program_handle;
      char *program_buffer, *program_log;
@@ -163,7 +163,7 @@ char *ReadKernelProgram() {
      return program_buffer;
 }
 
-// информация за платформата и устройствата
+// Информация за платформата и устройствата
 void PrintSystemInfo(cl_platform_id *platforms, cl_uint platforms_n) {
      char buffer[10240];
 
@@ -198,24 +198,23 @@ void PrintSystemInfo(cl_platform_id *platforms, cl_uint platforms_n) {
 }
 
 void ChangeColorPPM(PPMImage *img) {
-     // размер на изображението
-     unsigned int n = img->x * img->y;
-
-     // резултатен вектор
-     PPMPixel *host_result;
-
-     // входни данни за устройството
+     // Входни данни за устройството
      cl_mem device_pixel_data;
-     // изходни данни от устройството
+
+     // Изходни данни от устройството
      cl_mem device_result;
 
-     // OpenCL платформа
+     // Брой на платформите
      cl_uint platforms_n = 0;
+
+     // Списък на наличните платформи
      cl_platform_id platforms[100];
      clGetPlatformIDs(100, platforms, &platforms_n);
+
+     // Извод на наличните платформи и устройства
      PrintSystemInfo(platforms, platforms_n);
 
-     // Масив с устройства
+     // Списък на наличните устройства
      cl_device_id devices[100];
      cl_context context;
 
@@ -223,13 +222,19 @@ void ChangeColorPPM(PPMImage *img) {
      cl_command_queue cpu_queue;
 
      // Опашката на графичния ускорител
-     cl_command_queue gpu_queue;
+     cl_command_queue queue;
 
      cl_program program;
      cl_kernel kernel;
 
-     // определяне на размера на паметта
+     // Размер на изображението
+     unsigned int n = img->x * img->y;
+
+     // Определяне на размера на паметта
      size_t bytes = n*sizeof(PPMPixel);
+
+     // Резултатен вектор
+     PPMPixel *host_result;
 
      // Заделяне на памет в хоста
      host_result = (PPMPixel*)malloc(bytes);
@@ -237,37 +242,31 @@ void ChangeColorPPM(PPMImage *img) {
      size_t globalSize, localSize;
      cl_int err;
      
-     // размерност на локална група
+     // Размерност на локална група
      localSize = 64;
      
-     // брой локални групи
+     // Брой локални групи
      globalSize = ceil(n/(float)localSize)*localSize;
      
-     // списък на наличните платформи
-     // cl_int num_platforms;
-     // err = clGetPlatformIDs(2, cpPlatform, &num_platforms);
-     
-     // брой налични устройства
+     // Брой налични устройства
      cl_uint num_devices_returned;
 
-     // задаване на 
+     // Извличане на наличните устройства в devices
      err = clGetDeviceIDs(platforms[1], CL_DEVICE_TYPE_GPU, 100, devices, &num_devices_returned);
-     // err = clGetDeviceIDs(cpPlatform, CL_DEVICE_TYPE_CPU, 1, &devices[1], &num_devices_returned);
      
-     // създаване на контекст  
+     // Създаване на контекст от първото устройство в devices
      context = clCreateContext(0, 1, &devices[0], NULL, NULL, &err);
      
-     // създаване на опашки 
-     gpu_queue = clCreateCommandQueue(context, devices[0], 0, &err);
-     // cpu_queue = clCreateCommandQueue(context, devices[1], 0, &err);
+     // Създаване на опашки 
+     queue = clCreateCommandQueue(context, devices[0], 0, &err);
      
-     // четене на kernel функцията от външен файл
+     // Четене на kernel функцията от външен файл
      char *program_buffer = ReadKernelProgram();
 
-     // създаване на програма на базата на прочетената kernel функция
+     // Създаване на програма на базата на прочетената kernel функция
      program = clCreateProgramWithSource(context, 1, (const char **) & program_buffer, NULL, &err);
      
-     // компилиране на kernel функцията
+     // Компилиране на kernel функцията
      err = clBuildProgram(program, 0, NULL, NULL, NULL, NULL);
      if (err) {
           char log[10240] = "";
@@ -275,43 +274,43 @@ void ChangeColorPPM(PPMImage *img) {
           printf("Program build log:\n%s\n", log);
      }
      
-     // създаване на изчисляващата kernel функция в програмата, която ще се изпълни
+     // Създаване на изчисляващата kernel функция в програмата, която ще се изпълни
      kernel = clCreateKernel(program, "grayscale", &err);
      
-     // създаване на входните и изходните масиви в устройството за изчисленията
+     // Създаване на входните и изходните масиви в устройството за изчисленията
      device_pixel_data = clCreateBuffer(context, CL_MEM_READ_ONLY, bytes, NULL, NULL);
      device_result = clCreateBuffer(context, CL_MEM_WRITE_ONLY, bytes, NULL, NULL);
      
-     // запис на данните във входния масив в изчисляващото устройство
-     err = clEnqueueWriteBuffer(gpu_queue, device_pixel_data, CL_TRUE, 0, bytes, img->data, 0, NULL, NULL);
+     // Запис на данните във входния масив в изчисляващото устройство
+     err = clEnqueueWriteBuffer(queue, device_pixel_data, CL_TRUE, 0, bytes, img->data, 0, NULL, NULL);
 
-     // задаване на аргументите на kernel функцията
+     // Задаване на аргументите на kernel функцията
      err  = clSetKernelArg(kernel, 0, sizeof(cl_mem), &device_pixel_data);
      err |= clSetKernelArg(kernel, 1, sizeof(cl_mem), &device_result);
      err |= clSetKernelArg(kernel, 2, sizeof(unsigned int), &n);
      
-     // изпълнение на kernel функцията върху входните данните 
-     err = clEnqueueNDRangeKernel(gpu_queue, kernel, 1, NULL, &globalSize, &localSize, 0, NULL, NULL);
+     // Изпълнение на kernel функцията върху входните данните 
+     err = clEnqueueNDRangeKernel(queue, kernel, 1, NULL, &globalSize, &localSize, 0, NULL, NULL);
      
-     // изчакаване за завършване на обработката на опашката
-     clFinish(gpu_queue);
+     // Изчакаване за завършване на обработката на опашката
+     clFinish(queue);
      
-     // четене на резултата от устройството
-     clEnqueueReadBuffer(gpu_queue, device_result, CL_TRUE, 0, bytes, host_result, 0, NULL, NULL );
+     // Четене на резултата от устройството
+     clEnqueueReadBuffer(queue, device_result, CL_TRUE, 0, bytes, host_result, 0, NULL, NULL );
      
      int i;
      for (i = 0; i < n; i++)
           img->data[i] = host_result[i];
  
-    // освобождаване на OpenCL resources
+    // Освобождаване на OpenCL resources
     clReleaseMemObject(device_pixel_data);
     clReleaseMemObject(device_result);
     clReleaseProgram(program);
     clReleaseKernel(kernel);
-    clReleaseCommandQueue(gpu_queue);
+    clReleaseCommandQueue(queue);
     clReleaseContext(context);
  
-    // освобождаване на хост ресурси
+    // Освобождаване на хост ресурси
     free(host_result);
 }
 
