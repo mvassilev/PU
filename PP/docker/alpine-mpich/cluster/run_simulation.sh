d6eacdbff772267059d7501f273d6654a2e0a359
Martin Vassilev <mrtn.vassilev@gmail.com>
2023-03-03 09:44:40 +0200

    Add node count parameter for generating the gif.

--- PP/docker/alpine-mpich/cluster/run_simulation.sh
+++ PP/docker/alpine-mpich/cluster/run_simulation.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Workflow script for particle sim
-# sh run_simulation.sh 50 35 for 50 particles for 35 timesteps
+# sh run_simulation.sh 50 35 4 for 50 particles for 35 timesteps for 4 nodes
 #cd $DATA
 
 #execute code
@@ -18,20 +18,47 @@ for ((i = 0; i < ${#containers[@]}; ++i)); do
   echo ================================
 done
 
-#performn visualization
+plot=""
+i=0;
+while (($i <= $3-1))
+do
+    plot+="\"timedat.$i\" i $i u 4:5 pt 3  ps 1 t \"Node $i\""
+
+    if [[ $i < $(($3-1)) ]]; then
+      plot+=", "
+    else
+      plot+=";"
+    fi
+
+    let i=$i+1
+done
+
 i=0;
 while (($i <= $2-1))
 do
-    echo "set xrange [0:1]
+    printf "set xrange [0:1]
     set title \"$1 particles at timestep $i\"
     set yrange [0:1]
     set grid
     set term gif size 320,480
     set output '$i.gif'
-    plot \"timedat.0\" i $i u 4:5 pt 3  ps 1 t \"Node 0\", \"timedat.1\" i $i u 4:5 pt 3  ps 1 t \"Node 1\",\"timedat.2\" i $i u 4:5 pt 3 ps 1 t \"Node 2\", \"timedat.3\" i $i u 4:5 pt 3  ps 1 t \"Node 3\";" >data_$i.gnu
+    plot " > data_$i.gnu
+    j=0
+    while (($j <= $3-1))
+    do
+      printf "\"timedat.$j\" i $i u 4:5 pt 3  ps 1 t \"Node $j\"" >> data_$i.gnu
+      if [[ $j < $(($3-1)) ]]; then
+        printf ", " >> data_$i.gnu
+      else
+        printf ";" >> data_$i.gnu
+      fi
+      let j=j+1
+    done
     gnuplot data_$i.gnu
     let i=$i+1
 done
+
+rm animation.gif
 
 # Generating animated file
 echo "Generating animated file"
